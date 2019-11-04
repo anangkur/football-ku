d7ab498738612ef6bf73265e8bd7528dd0271734
anangkur <anang.kur@gits.id>
2019-11-04 22:39:58 +0700

    create activity league.

---
+++ app/src/main/java/com/anangkur/kotlinexpertsubmission/feature/league/LeagueActivity.kt
@@ -0,0 +1,13 @@
+package com.anangkur.kotlinexpertsubmission.feature.league
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anangkur.kotlinexpertsubmission.R
+
+class LeagueActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_league)
+    }
+}
