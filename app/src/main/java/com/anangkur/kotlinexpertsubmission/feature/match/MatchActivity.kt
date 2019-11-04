4f7d0a9d79baaa4a65eb425ed3c09b9635ca9f1c
anangkur <anang.kur@gits.id>
2019-11-04 22:04:23 +0700

    create activity match.

--- app/src/main/java/com/anangkur/kotlinexpertsubmission/feature/match/MatchActivity.kt
+++ app/src/main/java/com/anangkur/kotlinexpertsubmission/feature/match/MatchActivity.kt
@@ -2,12 +2,35 @@ package com.anangkur.kotlinexpertsubmission.feature.match
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.anangkur.kotlinexpertsubmission.R
+import com.anangkur.kotlinexpertsubmission.util.showToastShort
+import kotlinx.android.synthetic.main.activity_match.*
 
 class MatchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_match)
+
+        setupToolbar()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_team, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.menu_team -> showToastShort(this, getString(R.string.app_name))
+        }
+        return true
+    }
+
+    private fun setupToolbar(){
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = ""
     }
 }
