956410ecea10cce5b2e70b930f049082e6a1546b
anangkur <anang.kur@gits.id>
2019-11-23 21:59:24 +0700

    create unit test for next and prev match viewmodel.

---
+++ app/src/test/java/com/anangkur/kotlinexpertsubmission/feature/leagueDetail/prevMatch/PrevMatchViewModelTest.kt
@@ -0,0 +1,58 @@
+package com.anangkur.kotlinexpertsubmission.feature.leagueDetail.prevMatch
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.anangkur.kotlinexpertsubmission.data.Repository
+import com.anangkur.kotlinexpertsubmission.data.model.Event
+import com.anangkur.kotlinexpertsubmission.data.model.ResponseMatch
+import com.anangkur.kotlinexpertsubmission.data.model.Result
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import junit.framework.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.Rule
+import org.mockito.ArgumentCaptor
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+
+class PrevMatchViewModelTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+    private lateinit var viewModel: PrevMatchViewModel
+    @Mock
+    private lateinit var repository: Repository
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        viewModel = PrevMatchViewModel(repository)
+    }
+
+    @Test
+    fun getPrevMatch() {
+        val fakeData = createDummyResult()
+        val response = MutableLiveData<Result<ResponseMatch>>()
+        response.value = fakeData
+
+        whenever(repository.getPrevMatch("")).thenReturn(response)
+
+        val observer = mock<Observer<Result<ResponseMatch>>>()
+        viewModel.getPrevMatch().observeForever(observer)
+        viewModel.refreshData()
+        val captor = ArgumentCaptor.forClass(Result::class.java)
+        captor.run {
+            verify(observer).onChanged(capture() as Result<ResponseMatch>?)
+            Assert.assertNotNull(value)
+        }
+    }
+
+    private fun createDummyResult(): Result<ResponseMatch> {
+        val eventList = ArrayList<Event>()
+        eventList.add(Event())
+        return Result.success(ResponseMatch(eventList))
+    }
+}
